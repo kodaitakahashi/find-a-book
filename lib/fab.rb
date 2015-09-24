5b560797a7f142fdd9631b46cf7e8e4ff8557751
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2015-09-24 23:56:26 +0900

    systemID取得用のJSONの取得ができた

--- lib/fab.rb
+++ lib/fab.rb
@@ -1,5 +1,37 @@
+#!/usr/local/env ruby
+#coding:utf-8
 require "fab/version"
+require "apikey"
+require "json"
+require "open-uri"
+require "uri"
 
 module Fab
-  # Your code goes here...
+
+  class Find
+
+  end
+  
+  class Set < Find
+    def initialize()
+      @send_param = "http://api.calil.jp/library?appkey=#{Fab::Apikey::APIKEY}"
+    end
+    
+    def set_param(pref,city)
+      @send_param << "&pref=#{pref}"
+      @send_param << "&city=#{city}"
+      @send_param << "&format=json"
+    end
+
+    def get_id()
+      encode_uri = URI.encode(@send_param)
+      get_json = open(encode_uri).read
+      get_json.gsub!("callback([","")
+      get_json.gsub!("]);","")
+      result = JSON.load(get_json)
+      p result
+#      @library_list |= Array.new
+      
+    end
+  end
 end
